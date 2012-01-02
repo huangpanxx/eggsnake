711456cea0639a2c36da9092b6cd6838dc9cc27b
Ruby <huangpanxx@yahoo.com.cn>
2012-01-02 21:36:02 +0800

    创建项目框架,请不要再修改Android项目的代码

--- eggsnake/src/com/maple/eggsnake/Game.java
+++ eggsnake/src/com/maple/eggsnake/Game.java
@@ -1,34 +1,15 @@
 /* 
- * Description	: GameListener
+ * Description	: GameEntry
  * Author		: 黄攀
  * Created		: 2012-1-2
  */
 
 package com.maple.eggsnake;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game implements ApplicationListener {
-	
-	public void create() {
-	}
-	
-	public void render() {
-		
-	}
-	public void resize(int width,int height) {
-		
-	}
-	public void  pause() {
-		
-	}
-	public void  resume() {
-		
-	}
-	public void dispose() {
-		
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+
+public class Game {
+	public static void main(String args[]){
+		new JoglApplication(new GameListener(),"EggSnake",480,320,true);
 	}
 }
