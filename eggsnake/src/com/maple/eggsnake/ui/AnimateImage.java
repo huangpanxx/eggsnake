f87a735818b51e943321425a30eb12bd1b22d0c9
zhujianxin <411063423@qq.com>
2012-01-04 10:58:57 +0800

    更新后的AnimateImage，修复了不能永久旋转的Bug

--- eggsnake/src/com/maple/eggsnake/ui/AnimateImage.java
+++ eggsnake/src/com/maple/eggsnake/ui/AnimateImage.java
@@ -2,26 +2,27 @@ package com.maple.eggsnake.ui;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Forever;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateBy;
 import com.badlogic.gdx.scenes.scene2d.actions.RotateTo;
 import com.badlogic.gdx.scenes.scene2d.actors.Image;
 
 /**
  * @author zhiwei.wang
  * @version 0.0
- * @deprecated 这是一个旋转控件（可以理解为按钮）
  * @created 03-一月-2012 17:34:18
  */
 
-public class AnimateImage extends Image {
-
-	private Texture rotateTexture; // 旋转控件的图片
-	private float rotateDuration = 4f; // 旋转间隔
-	private RotateDirection rotateDirection;// 旋转方向
-	// private MyGame myGame; //游戏主控
-	private boolean EnabledSwitchSreen; // 是否允许切换场景
-
-	private float startX; // 玩家触控起的始点横坐标
-	private float startY; // 玩家触控的起始点纵坐标
+public class AnimateImage extends Image{
+	
+	private Texture rotateTexture;			//旋转控件的图片
+	private float rotateDuration = 4f;      //旋转间隔
+	private RotateDirection rotateDirection;//旋转方向
+	//private MyGame myGame;                    //游戏主控
+	private boolean EnabledSwitchSreen;     //是否允许切换场景
+	
+	private float startX; //玩家触控起的始点横坐标
+	private float startY; //玩家触控的起始点纵坐标
 
 	public AnimateImage(String name, Texture texture, float duration,
 			RotateDirection direction) {
@@ -33,36 +34,35 @@ public class AnimateImage extends Image {
 		this.rotateDirection = direction;
 		this.initActions();
 	}
-
+	
 	@Override
 	public boolean touchDown(float x, float y, int pointer) {
 		this.startX = x;
 		this.startY = y;
-		System.out.println("startX: " + x + " startY: " + y + " pointer: "
-				+ pointer);
-		return touchable;
+		System.out.println("startX: " + x + " startY: " + y + " pointer: " + pointer);
+		return touchable;	
 	}
-
+	
 	@Override
-	public void touchDragged(float x, float y, int pointer) {
+	public void touchDragged(float x, float y, int pointer){
 		System.out.println("This image is being dragged!");
 	}
-
+	
 	@Override
-	public void touchUp(float x, float y, int pointer) {
+	public void touchUp(float x, float y, int pointer){
 		System.out.println("upX: " + x + " upY: " + y + " pointer:" + pointer);
-		if ((10f <= (x - startX)) && (10f <= (y - startY)))
+		if((10f <= (x - startX)) && (10f <= (y - startY)))
 			;
 	}
 
-	/* 初始化该控件的旋转动作 */
-	private void initActions() {
+	/*初始化该控件的旋转动作*/
+	private void initActions(){
 		Action rotateAction;
-		if (RotateDirection.CLOCKWISE == this.rotateDirection)
-			rotateAction = RotateTo.$(360, this.rotateDuration);
+		if(RotateDirection.CLOCKWISE == this.rotateDirection)
+			rotateAction = RotateBy.$(360, this.rotateDuration);
 		else
-			rotateAction = RotateTo.$(-360, this.rotateDuration);
-		this.action(rotateAction);
+			rotateAction = RotateBy.$(-360, this.rotateDuration);
+		this.action(Forever.$(rotateAction));	
 	}
 
 	/**
@@ -73,8 +73,7 @@ public class AnimateImage extends Image {
 	}
 
 	/**
-	 * @param rotateTexture
-	 *            the rotateTexture to set
+	 * @param rotateTexture the rotateTexture to set
 	 */
 	public void setRotateTexture(Texture rotateTexture) {
 		this.rotateTexture = rotateTexture;
@@ -88,8 +87,7 @@ public class AnimateImage extends Image {
 	}
 
 	/**
-	 * @param rotateDuration
-	 *            the rotateDuration to set
+	 * @param rotateDuration the rotateDuration to set
 	 */
 	public void setRotateDuration(float rotateDuration) {
 		this.rotateDuration = rotateDuration;
@@ -103,24 +101,9 @@ public class AnimateImage extends Image {
 	}
 
 	/**
-	 * @param rotateDirection
-	 *            the rotateDirection to set
+	 * @param rotateDirection the rotateDirection to set
 	 */
 	public void setRotateDirection(RotateDirection rotateDirection) {
 		this.rotateDirection = rotateDirection;
-	}
-
-	/**
-	 * @return the enabledSwitchSreen
-	 */
-	public boolean isEnabledSwitchSreen() {
-		return EnabledSwitchSreen;
-	}
-
-	/**
-	 * @param enabledSwitchSreen the enabledSwitchSreen to set
-	 */
-	public void setEnabledSwitchSreen(boolean enabledSwitchSreen) {
-		EnabledSwitchSreen = enabledSwitchSreen;
 	}
 }
