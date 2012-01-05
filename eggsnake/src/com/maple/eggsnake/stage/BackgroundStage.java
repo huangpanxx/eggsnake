91d318e9440bd2f0387d4ad7065a2a54a2fdb1e4
Ruby <huangpanxx@yahoo.com.cn>
2012-01-05 17:19:04 +0800

    清理Test代码

--- eggsnake/src/com/maple/eggsnake/stage/BackgroundStage.java
+++ eggsnake/src/com/maple/eggsnake/stage/BackgroundStage.java
@@ -1,5 +1,9 @@
 package com.maple.eggsnake.stage;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.maple.eggsnake.actor.ui.ActorRegister;
 import com.maple.eggsnake.logger.DefaultLogger;
 import com.maple.eggsnake.logger.Loggable;
 import com.maple.eggsnake.screen.NavigateScreen;
@@ -9,30 +13,31 @@ public class BackgroundStage extends BaseStage {
 	final Loggable logger;
 	NavigateScreen screen;
 
-	public BackgroundStage(NavigateScreen _screen, float width,
-			float height, boolean stretch) {
+	public BackgroundStage(NavigateScreen _screen, float width, float height,
+			boolean stretch) {
 		super(width, height, stretch);
 		logger = DefaultLogger.getDefaultLogger();
 		loadBackground();
 	}
 
 	private void loadBackground() {
-		
+		FileHandle fileHandle = Gdx.files.internal("data/images/background.png");
+		ActorRegister.singleRegister(this, new Texture(fileHandle), 0, 0);
 	}
 
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void resume() {
@@ -41,7 +46,7 @@ public class BackgroundStage extends BaseStage {
 
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- eggsnake/src/com/maple/eggsnake/stage/ContentStageTest.java
+++ eggsnake/src/com/maple/eggsnake/stage/ContentStageTest.java
@@ -7,68 +7,41 @@
 package com.maple.eggsnake.stage;
 
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.maple.eggsnake.actor.ui.AnimateImage;
-import com.maple.eggsnake.actor.ui.AnimateImageEvent;
-import com.maple.eggsnake.actor.ui.AnimateImageListener;
-import com.maple.eggsnake.actor.ui.RotateDirection;
-import com.maple.eggsnake.logger.DefaultLogger;
-import com.maple.eggsnake.logger.Loggable;
-import com.maple.eggsnake.screen.NavigateScreen;
 
 public class ContentStageTest extends BaseStage {
-	final Loggable logger;
-	AnimateImage btnStart;
-	NavigateScreen screen;
 
 	public ContentStageTest(Screen _screen, float width, float height,
 			boolean stretch) {
 		super(width, height, stretch);
-		logger = DefaultLogger.getDefaultLogger();
-		createButtons();
-	}
-
-	private void createButtons() {
-		btnStart = new AnimateImage("Start", new Texture(
-				"data/images/bottle.png"), 1f, RotateDirection.ANTICLOCKWISE);
-		btnStart.addActionListener(new AnimateImageListener() {
-			@Override
-			public void onTouchDragged(AnimateImageEvent e) {
-				logger.log("Btn dragged");
-			}
-		});
-		logger.log("Content Stage called");
-		this.addActor(btnStart);
-		
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
