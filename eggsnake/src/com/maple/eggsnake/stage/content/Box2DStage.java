caa23e3d4c02d730e94de012adb086793f82b2ee
zhujianxin <411063423@qq.com>
2012-01-08 19:55:18 +0800

    描述：略

--- eggsnake/src/com/maple/eggsnake/stage/content/Box2DStage.java
+++ eggsnake/src/com/maple/eggsnake/stage/content/Box2DStage.java
@@ -1,8 +1,15 @@
 package com.maple.eggsnake.stage.content;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -13,6 +20,9 @@ import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.actors.Image;
+import com.maple.eggsnake.actor.ui.ActorRegister;
+import com.maple.eggsnake.logger.DefaultLogger;
 import com.maple.eggsnake.physics.B2WorldFactory;
 import com.maple.eggsnake.service.ApplicationService;
 import com.maple.eggsnake.stage.BaseStage;
@@ -108,7 +118,7 @@ public class Box2DStage extends BaseStage {
 		this.camera.position.set(0,24,1);
 		world.step(Gdx.graphics.getDeltaTime(), 10, 10);
 		render.render(world, this.camera.combined);
-
+//		}
 	}
 
 	@Override
