1f3a4fa2b0f0a0b2f8232ccf5e65f090dbf19e2a
Ruby <huangpanxx@yahoo.com.cn>
2012-01-09 23:07:06 +0800

    修正地图索引文件在Android的加载错误

--- eggsnake/src/com/maple/eggsnake/service/GateLoader.java
+++ eggsnake/src/com/maple/eggsnake/service/GateLoader.java
@@ -1,25 +1,35 @@
 package com.maple.eggsnake.service;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.World;
 import com.google.gson.Gson;
-import com.maple.eggsnake.util.FileHelper;
+import com.maple.eggsnake.logger.DefaultLogger;
+import com.maple.eggsnake.logger.Loggable;
 import com.maple.eggsnake.util.PathHelper;
 
 public class GateLoader {
-	
+
 	String baseDir = null;
 	MapDictionary mapInfo = null;
-	
-	public GateLoader(String baseDir,String dictionaryFile) throws Exception{
+	Loggable logger;
+
+	public GateLoader(String baseDir, String dictionaryFile) throws Exception {
+		logger = DefaultLogger.getDefaultLogger();
 		this.baseDir = baseDir;
-		String text = FileHelper.readAll(PathHelper.combine(baseDir, dictionaryFile));
+
+		logger.logWithSignature(this, "加载关卡文件 ：%1$s",
+				PathHelper.combine(baseDir, dictionaryFile));
+		String absPath = PathHelper.combine(this.baseDir, dictionaryFile);
+		String text = Gdx.files.internal(absPath).readString();
+		logger.logWithSignature(this, "关卡Json:", text);
 		Gson gson = new Gson();
 		mapInfo = gson.fromJson(text, MapDictionary.class);
 	}
-	
-	public World loadGate(int gateIndex){
+
+	public World loadGate(int gateIndex) {
+		logger.logWithSignature(this, "加载地图号:%1$s", gateIndex);
 		String path = mapInfo.Maps.get(gateIndex);
-//		String absPath = PathHelper.combine(baseDir, path);
+		logger.logWithSignature(this, "加载地图%1$s", path);
 		return ResourceLoader.loadWorld(path);
 	}
 }
