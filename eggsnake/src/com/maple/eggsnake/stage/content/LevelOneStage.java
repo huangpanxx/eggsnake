7e2f4934a6ea6a91b48500dafa264a53fc1e5e63
Ruby <huangpanxx@yahoo.com.cn>
2012-01-11 01:08:30 +0800

    去除冲突

--- eggsnake/src/com/maple/eggsnake/stage/content/LevelOneStage.java
+++ eggsnake/src/com/maple/eggsnake/stage/content/LevelOneStage.java
@@ -247,5 +247,11 @@ LogicalGameListener{
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void onMouseKilled(String mouseName) {
+		// TODO Auto-generated method stub
+		
+	}
 	}
 
