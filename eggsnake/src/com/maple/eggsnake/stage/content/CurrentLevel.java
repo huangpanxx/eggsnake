b00f01f2cba0b2cf2a8ccba782c4cf39285dd63e
Ruby <huangpanxx@yahoo.com.cn>
2012-01-23 21:02:55 +0800

    修正冲突，删除关卡文件

--- eggsnake/src/com/maple/eggsnake/stage/content/CurrentLevel.java
+++ eggsnake/src/com/maple/eggsnake/stage/content/CurrentLevel.java
@@ -4,11 +4,10 @@
  * @created		: 2012-1-6
  */
 
-<<<<<<< HEAD
+
 package com.maple.eggsnake.stage.content;
 
-=======
->>>>>>> origin/HEAD
+
 public class CurrentLevel {
 	public static int current = 0;
 }
